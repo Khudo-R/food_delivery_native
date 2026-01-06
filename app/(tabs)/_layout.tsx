c2b66e8dd0d065466d2f5fa5ba2d6e859eeba4ea
Khudo #R <mirkhud97@gmail.com>
2026-01-06 23:06:12 +0900

    updates

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Slot, Redirect } from "expo-router"
 
 export default function _Layout() {
-  const isAuthenticated = false; // Replace with your auth logic
+  const isAuthenticated = true; // Replace with your auth logic
   if (!isAuthenticated) return <Redirect href="/sign-in" /> 
   return <Slot />
 }
